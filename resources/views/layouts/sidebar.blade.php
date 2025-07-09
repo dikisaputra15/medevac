6f13233dc13d1ef86840c05a86fa37cc189c4f7f
Diki Saputra <diki@CNCRD.LAN>
2025-07-09 17:35:45 +0700

    update url dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
+    <a href="/home" class="brand-link">
       <img src="{{asset('AdminLTE')}}/dist/img/cclogo.jpeg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">MEDEVAC</span>
     </a>
@@ -16,7 +16,7 @@
                with font-awesome or any other icon font library -->
           <li class="nav-header">Hi, {{auth()->user()->name}}</li>
           <li class="nav-item {{ request()->is('/') ? 'menu-open' : '' }}">
-            <a href="/" class="nav-link">
+            <a href="/home" class="nav-link">
              <i class="bi bi-house-door-fill"></i>
               <p>
                 Dashboard
