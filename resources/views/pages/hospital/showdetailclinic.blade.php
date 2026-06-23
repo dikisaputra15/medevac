194ea56523b5c8c74c0ab53fbfba9f6bdffc306e
Diki Saputra <diki@CNCRD.LAN>
2026-06-23 11:19:16 +0700

    hapus other di clinical

--- resources/views/pages/hospital/showdetailclinic.blade.php
+++ resources/views/pages/hospital/showdetailclinic.blade.php
@@ -423,10 +423,6 @@
                         <td>Casuals</td>
                         <td>{{ $hospital->casuals }}</td>
                     </tr>
-                    <tr>
-                        <td>Others</td>
-                        <td>{{ $hospital->others }}</td>
-                    </tr>
                 </table>
                 </div>
                 </div>
